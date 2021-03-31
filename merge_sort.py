00514dd71e491108191cdc67e22919384296fedb
Natallia <75850543+nattie0@users.noreply.github.com>
2021-03-31 19:42:02 +0300

    Create merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,27 @@
+def merge_sort(arr):
+	if len(arr) <= 1:
+		return arr
+	index = len(arr) // 2
+	left = arr[:index]
+	right = arr[index:]
+	return merge(merge_sort(left), merge_sort(right))
+	
+def merge(left, right):
+	left_max = len(left)
+	right_max = len(right)
+	left_cur, right_cur = 0, 0
+	result = []
+	while left_cur<left_max and right_cur < right_max:
+		if left[left_cur] <= right[right_cur]:
+			result.append(left[left_cur])
+			left_cur += 1
+		else:
+			result.append(right[right_cur])
+			right_cur += 1
+	if left_cur < left_max:
+		result.extend(left[left_cur:])
+	else: result.extend(right[right_cur:])
+	return result
+	
+arr = [4,7,5,3,9,5,1,0]
+print(merge_sort(arr))
